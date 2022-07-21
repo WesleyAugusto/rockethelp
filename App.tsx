990f48ec08ec0fcc25a145d8368444a7586a5f38
WesleyAugusto <ranzerap@gmail.com>
2022-07-21 20:28:58 -0300

    signIn

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
+import { NativeBaseProvider } from 'native-base';
 import {SignIn} from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
+import {useFonts,Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular,Roboto_700Bold})
 
   return (
-  <SignIn />
+    <NativeBaseProvider theme={THEME}> 
+  { fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+
+
 
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,14 @@
-import{View,Text} from 'react-native'
+import{VStack, Heading} from 'native-base';
+import { background } from 'native-base/lib/typescript/theme/styled-system';
 
 export function SignIn (){
 return(
-    <View>
-        <Text> olá tixti </Text>
-    </View>
+    <VStack flex={1} alignItems ="center" bg="gray.600" px={8} pt={24}>
+    
+    <Heading color="green.400" fontSize="xl" mt="6" mb="1/2">
+        acesse sua conta 
+    </Heading>
+    </VStack>
 
 
 )
